cad9bc229afe312793074f79efe8608890fb30d9
Tomas Trajan <tomas.trajan@gmail.com>
2013-09-28 20:59:04 +0200

    Reworked to be implemented as Object literal notation, all functions are now stored in simpleTimelineJs object

--- js/timeline.js
+++ js/timeline.js
@@ -1,91 +1,131 @@
-/* 	SIMPLE TIMELINE  by Trajan 2013 */
+/* 	SimpleTimelineJs by Trajan 2013 */
 	
-var id = 0;
-var yearPx = 84;
-var monthPx = 7;
-var baseOffset = 42;
-
-var date = new Date();
-var currentYear = date.getFullYear(); 
-var currentMonth = date.getMonth() + 1;
-
-var timelineStartYear;
-var timelineEndYear;
-
-function generateYears(startYear, endYear) {
-	timelineStartYear = startYear;
-	timelineEndYear = endYear;
-	var yearCount = timelineEndYear - timelineStartYear;
+var simpleTimelineJs = {
+    
+	currentYear : function() {
+		simpleTimelineJs.config.now.getFullYear();
+	},
 	
-	$(".timelineYears").children().remove();
-	var width = $(document).width();
-	for (i = 0; i <= yearCount; i++) {
-		$(".timelineYears").append("<li>" + (+timelineStartYear + +i) +"</li>");
+	currentMonth : function() {
+		simpleTimelineJs.config.now.getMonth() + 1;
+	},
+	
+	config : {
+		yearPxWidth : 84,
+		monthPxWidth : 7,
+		basePxOffset : 42,
+		timelineId : 0,
+		now : new Date(),
+		timelineStartYear : 0,
+		timelineEndYEar : 0
+	},
+	
+	element : {
+		app : "#simpleTimelineJs",
+		holder : "#simpleTimelineJs_FullWidthHolder",
+		timelines : "#simpleTimelineJs_timelines",
+		timeline : ".simpleTimelineJs_timeline",
+		tooltip : ".simpleTimelineJs_Tooltip"
+	},
+	
+	api :  {
+		
+		initTimeline : function(startYear, endYear) {
+			
+			simpleTimelineJs.api.prepareTimelineTemplate();
+			simpleTimelineJs.api.prepareTimelineEvents();
+			
+			simpleTimelineJs.config.timelineStartYear = startYear;
+			simpleTimelineJs.config.timelineEndYear = endYear;
+			
+			var yearCount = endYear - startYear;
+			var width = $(document).width();
+			
+			$(simpleTimelineJs.element.holder).find("ul").children().remove();
+		
+			for (i = 0; i <= yearCount; i++) {
+				$(simpleTimelineJs.element.holder).find("ul").append("<li>" + (+simpleTimelineJs.config.timelineStartYear + +i) +"</li>");
+			}
+			$(simpleTimelineJs.element.holder).css("width", (i * simpleTimelineJs.config.yearPxWidth) + "px");
+		},
+		
+		prepareTimelineTemplate: function() { 	
+			$(simpleTimelineJs.element.app).append("<div id='simpleTimelineJs_FullWidthHolder'></div>");
+			
+			$(simpleTimelineJs.element.holder).append("<ul class='simpleTimelineJs_years'></ul>");
+			$(simpleTimelineJs.element.holder).append("<br style='clear:both;' />");
+			
+			$(simpleTimelineJs.element.holder).append("<div id='simpleTimelineJs_timelines'></div>")
+			$(simpleTimelineJs.element.timelines).append("<div></div>");
+			$(simpleTimelineJs.element.timelines).append("<br style='clear:both;' />");
+					
+			$(simpleTimelineJs.element.holder).append("<ul class='simpleTimelineJs_years simpleTimelineJs_yearsBottom'></ul>");
+			$(simpleTimelineJs.element.holder).append("<br style='clear:both;' />");
+		},
+		
+		prepareTimelineEvents: function() {
+		
+			$(simpleTimelineJs.element.app).on( "mouseenter", simpleTimelineJs.element.timeline, function() {
+				var content = $(this).find("p").html();
+				$("body").append("<div class='simpleTimelineJs_Tooltip'><p>" + content + "</p></div>")
+				
+				var parentTop = $(this).offset().top;
+				var parentLeft = $(this).offset().left;
+				var parentHeight = $(this).height();
+				var height = $(".simpleTimelineJs_Tooltip").height();
+				var monthPx = simpleTimelineJs.config.monthPxWidth;
+				var top = parentTop - parentHeight - height - monthPx - 2;
+				
+				$(simpleTimelineJs.element.tooltip).css("left", parentLeft + monthPx  + "px");
+				$(simpleTimelineJs.element.tooltip).css("top",  top + "px");
+			});
+
+			$(simpleTimelineJs.element.app).on( "mouseleave", simpleTimelineJs.element.timeline, function() {
+				$(simpleTimelineJs.element.tooltip).remove();
+			});
+		
+		},
+		
+		addTimelinePeriod : function(name, color, startYear, startMonth, endYear, endMonth, newTimeline) {
+			var offset = simpleTimelineJs.config.basePxOffset;
+			var yearPx = simpleTimelineJs.config.yearPxWidth;
+			var monthPx = simpleTimelineJs.config.monthPxWidth;
+			var timelineStartYear = simpleTimelineJs.config.timelineStartYear;
+			var id = simpleTimelineJs.config.timelineId;
+			
+			var periodLeft = (offset + ((startYear - timelineStartYear) * yearPx)) + ((startMonth - 1) * monthPx);
+			var periodRight = (offset + ((endYear - timelineStartYear) * yearPx)) + (endMonth * monthPx);
+			var periodWidth = periodRight - periodLeft;
+			
+			$(simpleTimelineJs.element.timelines + " div").first()
+				.append("<div class='simpleTimelineJs_timeline period" + id + "'></div>");
+				
+			if(newTimeline == true) {
+				$("." + "period" + id).before("<br style='clear:both;' />");
+				$("." + "period" + id).css("margin-left", periodLeft + "px");
+			} else {
+				var prevLeft = $("." + "period" + id).prev().offset().left;
+				var prevWidth = $("." + "period" + id).prev().css("width").replace("px", "");
+				var prevEnd = +prevLeft + +prevWidth;
+				var holderLeft = $(simpleTimelineJs.element.holder).offset().left;
+				periodLeft = +periodLeft + +holderLeft - +prevEnd;
+				
+				$("." + "period" + id).css("margin-left", periodLeft + "px");
+			}	
+
+			var yearString = (startYear == endYear ? startYear : startYear + " - " + endYear);
+			
+			$("." + "period" + id).append("<div></div>");
+			$("." + "period" + id).append("<p>" + name + "<br /><span style='color:#" + color + "'>" 
+				+ yearString +"</span></p");
+			
+			$("." + "period" + id).css("width", periodWidth + "px");
+			$("." + "period" + id).find("div").css("background-color", "#" + color);
+			
+			simpleTimelineJs.config.timelineId++;
+		}
 	}
-	$("#timelineFullWidthHolder").css("width", (i * yearPx) + "px");
-}
+};
 
-function setTimelineElement(name, color, startYear, startMonth, endYear, endMonth, newTimeline) {
-	
-	var elemLeft = (baseOffset + ((startYear - timelineStartYear) * yearPx)) + ((startMonth - 1) * monthPx);
-	var elemRight = (baseOffset + ((endYear - timelineStartYear) * yearPx)) + (endMonth * monthPx);
-	var elemWidth = elemRight - elemLeft;
-	
-	$("#timelines div").first().append("<div class='timeline " + "elem" + id + "'></div>");
-		
-	if(newTimeline == true) {
-	
-		$("." + "elem" + id).before("<br class='clearFix' />");
-		$("." + "elem" + id).css("float", "left");
-		$("." + "elem" + id).css("margin-left", elemLeft + "px");
-		
-	} else {
-		
-		var prevLeft = $("." + "elem" + id).prev().offset().left;
-		var prevWidth = $("." + "elem" + id).prev().css("width").replace("px", "");
-		var prevEnd = +prevLeft + +prevWidth;
-		var holderLeft = $("#timelineFullWidthHolder").offset().left;
-		elemLeft = +elemLeft + +holderLeft - +prevEnd;
-		
-		$("." + "elem" + id).css("margin-left", elemLeft + "px");
-		$("." + "elem" + id).css("float", "left");
-	}
-	
-	var yearString;
-	if(startYear == endYear) {
-		yearString = startYear;
-	} else {
-		yearString = startYear + " - " + endYear
-	}
-	
-	/* ADD BACKGROUND AND LABEL */
-	$("." + "elem" + id).append("<div></div>");
-	$("." + "elem" + id).append("<p>" + name + "<br /><span style='color:#" + color + "'>" 
-		+ yearString +"</span></p");
-	
-	/* APPLY CSS STYLES */
-	$("." + "elem" + id).css("width", elemWidth + "px");
-	$("." + "elem" + id).find("div").css("background-color", "#" + color);
-	
-	id++;
-}
-
-$( document ).on( "mouseenter", ".timeline", function() {
-	var content = $(this).find("p").html();
-	$("body").append("<div class='timelineTooltip shadow'><p>" + content + "</p></div>")
-	
-	var parentTop = $(this).offset().top;
-	var parentLeft = $(this).offset().left;
-	var parentHeight = $(this).height();
-	var height = $(".timelineTooltip").height();
-	var top = parentTop - parentHeight - height - monthPx - 2;
-	
-	$(".timelineTooltip").css("left", parentLeft + monthPx  + "px");
-	$(".timelineTooltip").css("top",  top + "px");
-});
-
-$( document ).on( "mouseleave", ".timeline", function() {
-	$(".timelineTooltip").remove();
-});
 
 
--- timeline_data.js
+++ timeline_data.js
@@ -1,7 +1,45 @@
 $(document).ready(function(){
 	
+	/* 
+		Initialize timeline and set year range
+		
+		simpleTimelineJs.api.initTimeline(timelineStartYear, timelineEndYear)
+	*/
+	
+	simpleTimelineJs.api.initTimeline(2003, 2014);
+	
 	/*
-		SOME NICE COLORS
+		Add timeline periods
+		
+		simpleTimelineJs.api.addTimelinePeriod(
+			"name of period", 
+			"color", 			- hexadecimal color representation e.g.: #ff0000
+			startYear, 			- number (eg: 2005)
+			startMonth, 		- number (eg: 11)
+			endYear, 			- number (eg: 2012)	or simpleTimelineJs.currentYear()
+			endMonth, 			- number (eg: 5) 	or simpleTimelineJs.currentMonth()
+			newTimeline			- boolean	true 	- 	period will start new timeline
+		);									false 	- 	period will be displayed inline 
+														with other periods in current timeline						
+	*/
+	
+	simpleTimelineJs.api.addTimelinePeriod("GOOD TIMES", 		"52af40", 2004, 01, 2006, 06, true);
+	simpleTimelineJs.api.addTimelinePeriod("THIS WAS AWESOME",	"27b3d1", 2006, 09, 2008, 04, false);
+	simpleTimelineJs.api.addTimelinePeriod("SOME PERIOD", 		"d18e2e", 2008, 09, 2011, 05, false);
+	simpleTimelineJs.api.addTimelinePeriod("FLASH EVENTS", 		"52af40", 2011, 07, 2011, 09, false);
+	
+	simpleTimelineJs.api.addTimelinePeriod("TROUBLE TIMES", 	"5f6ddf", 2007, 04, 2008, 08, true);
+	simpleTimelineJs.api.addTimelinePeriod("AFTERMATH",			"bd2e78", 2008, 10, 2009, 11, false);
+	simpleTimelineJs.api.addTimelinePeriod("REVIVAL",			"cf1e1e", 2013, 10, 2014, 02, false);
+	
+	simpleTimelineJs.api.addTimelinePeriod("INTERLUDE",			"d18e2e", 2003, 03, 2007, 11, true);
+	simpleTimelineJs.api.addTimelinePeriod("CONCURRENT EVENTS",	"474747", 2007, 12, 2010, 01, false);
+	
+	simpleTimelineJs.api.addTimelinePeriod("AGE OF STUFF",		"cf1e1e", 2005, 10, 2006, 12, true);
+	simpleTimelineJs.api.addTimelinePeriod("NEW ERA",			"52af40", 2007, 03, simpleTimelineJs.currentYear(), simpleTimelineJs.currentMonth(), false);
+	
+	/*
+		Some nice colors
 		
 		#52af40 - green
 		#27b3d1 - cyan
@@ -11,43 +49,5 @@ $(document).ready(function(){
 		#474747 - grey
 		#cf1e1e - red
 	*/
-	
-	
-	/* 
-		SET TIMELINE YEAR RANGE
-		
-		generateYears(timelineStartYear, timelineEndYear)
-	*/
-	generateYears(2001, 2016);
-	
-	
-	
-	/*
-		ADD TIMELINE EVENTS
-		
-		setTimelineElement("name of period", "color", startYear, startMonth, endYear, endMonth, newTimeline);
-	
-		@color 			- hexadecimal color representation e.g.: #ff0000
-		@newTimeline 	- boolean	true 	- 	element will start new timeline
-									false 	- 	element will be displayed inline 
-												with other elements in current timeline
-		@endYear 		- number (e.g.: 2012) or currentYear
-		@endMonth 		- number (e.g.: 5) or currentMonth	
-											
-	*/
-	setTimelineElement("GOOD TIMES", 		"52af40", 2004, 01, 2006, 06, true);
-	setTimelineElement("THIS WAS AWESOME",	"27b3d1", 2006, 09, 2008, 04, false);
-	setTimelineElement("SOME PERIOD", 		"d18e2e", 2008, 09, 2011, 05, false);
-	setTimelineElement("FLASH EVENTS", 		"52af40", 2011, 07, 2011, 09, false);
-	
-	setTimelineElement("TROUBLE TIMES", 	"5f6ddf", 2007, 04, 2008, 08, true);
-	setTimelineElement("AFTERMATH",			"bd2e78", 2008, 10, 2009, 11, false);
-	setTimelineElement("REVIVAL",			"cf1e1e", 2013, 10, 2014, 02, false);
-	
-	setTimelineElement("INTERLUDE",			"d18e2e", 2003, 03, 2007, 11, true);
-	setTimelineElement("CONCURRENT EVENTS",	"474747", 2007, 12, 2010, 01, false);
-	
-	setTimelineElement("AGE OF STUFF",		"cf1e1e", 2005, 10, 2006, 12, true);
-	setTimelineElement("NEW ERA",			"52af40", 2007, 03, currentYear, currentMonth, false);
 	
 });
